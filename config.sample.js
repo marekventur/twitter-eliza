74e9061d0b2e2fba1f91b067a1c3a08d31511eb3
marekventur <marekventur@gmail.com>
2014-10-14 21:14:54 +0100

    Add sample config

---
+++ config.sample.js
@@ -0,0 +1,13 @@
+module.exports = {
+	twitterCredentials: {
+	    consumer_key: 'xxx',
+	    consumer_secret: 'xxx',
+	    access_token_key: 'xxx',
+	    access_token_secret: 'xxx'
+    },
+    contextTimeout: 60 * 60 * 1000,
+    tweetReplySpeed: 100 * 1000,
+    maxContexts: 50,
+    search: ['#somehashtag'],
+    twitterUsername: 'xxx'
+}
